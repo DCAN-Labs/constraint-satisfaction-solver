37c5680f2924f0ec6d548cf2861b84f132ec635f
Paul Reiners <reine097@umn.edu>
2024-02-08 12:05:10 -0600

    Implemented get_legal_values_left_count and test.

---
+++ src/backtracking_search.py
@@ -0,0 +1,139 @@
+from abc import ABC, abstractmethod
+import operator
+
+
+class BacktrackingSearch(ABC):
+    def __init__(self, csp):
+        self.csp = csp
+
+    @abstractmethod
+    def select_unassigned_variable(self, assignment):
+        pass
+
+    @abstractmethod
+    def order_domain_values(self, var, assignment):
+        pass
+
+    def complete(self, assignment):
+        X = self.csp[0]
+        for x in X:
+            if x not in assignment.keys():
+                return False
+        return True
+
+    def backtrack(self, assignment):
+        if self.complete(assignment):
+            return assignment
+        var = self.select_unassigned_variable(assignment)
+        for value in self.order_domain_values(var, assignment):
+            inferences = dict()
+            if self.consistent(value, assignment):
+                assignment[var] = value
+                inferences = self.inference(var, value)
+                if inferences is not None:
+                    assignment.extend(inferences)
+                    result = self.backtrack(assignment)
+                    if result is not None:
+                        return result
+            assignment.pop(var)
+            for key in inferences.keys():
+                assignment.pop(key)
+        return None
+
+    def backtracking_search(self):
+        self.backtrack(dict())
+
+    @abstractmethod
+    def inference(self, var, value):
+        pass
+
+    def consistent(self, assignment):
+        vars = list(assignment.keys())
+        n = len(vars)
+        C = self.csp[2]
+        for i in range(n):
+            for j in range(n):
+                if i == j:
+                    continue
+                arc = (vars[i], vars[j])
+                if arc in C:
+                    valid_vals = C[arc]
+                    actual_vals = (assignment[arc[0]], assignment[arc[1]])
+                    if actual_vals not in valid_vals:
+                        return False
+        return True
+
+
+class AustraliaColoring(BacktrackingSearch):
+    def inference(self, var, value):
+        return None
+
+    def select_unassigned_variable(self, assignment):
+        X = self.csp[0]
+        min_remaining_values = -1
+        D = self.csp[1]
+        for x in X:
+            if x in assignment.keys():
+                continue
+            if min_remaining_values == -1 or len(D[x]) < min_remaining_values:
+                min_remaining_values = len(D[x])
+        keys_with_min = [key for key in D.keys() if len(D[key]) == min_remaining_values]
+        if len(keys_with_min) == 1:
+            return keys_with_min[0]
+        else:
+            max_degree = -1
+            max_degree_var = None
+            for x in X:
+                if x in assignment.keys():
+                    continue
+                degree = 0
+                C = self.csp[2]
+                for c in C.keys():
+                    if x == c[0] or x == c[1]:
+                        degree += 1
+                if max_degree == -1 or degree > max_degree:
+                    max_degree_var = x
+                    max_degree = degree
+            return max_degree_var
+
+    def get_neighboring_variables(self, var):
+        C = self.csp[2]
+        neighboring_variables = []
+        for c in C.keys():
+            if var == c[0]:
+                neighboring_variables.append(c[1])
+            elif var == c[1]:
+                neighboring_variables.append(c[0])
+        return neighboring_variables
+
+    def get_least_constraining_value(self, assignment, variable):
+        neighboring_variables = self.get_neighboring_variables(variable)
+        D = self.csp[1]
+        possible_values = D[variable]
+        value_to_constraint_factor = {d: 1 for d in D[variable]}
+        for value in possible_values:
+            extended_assignment = assignment.copy()
+            extended_assignment[variable] = value
+            for neighboring_variable in neighboring_variables:
+                legal_values_left = self.get_legal_values_left_count(extended_assignment, neighboring_variable)
+                value_to_constraint_factor[value] = value_to_constraint_factor[value] * legal_values_left
+        return value_to_constraint_factor
+
+    def order_domain_values(self, var, assignment):
+        sorted_d = \
+            dict(sorted(self.get_least_constraining_value(var, assignment), key=operator.itemgetter(1), reverse=True))
+
+        return sorted_d.keys()
+
+    def get_legal_values_left_count(self, assignment, neighboring_variable):
+        if neighboring_variable in assignment.keys():
+            return 1
+        else:
+            count = 0
+            D = self.csp[1]
+            for value in D[neighboring_variable]:
+                extended_assignment = assignment.copy()
+                extended_assignment[neighboring_variable] = value
+                if self.consistent(extended_assignment):
+                    count += 1
+            return count
---
+++ tests/test_backtracking_search.py
@@ -0,0 +1,64 @@
+from unittest import TestCase
+
+from backtracking_search import AustraliaColoring
+
+
+class TestAustraliaColoring(TestCase):
+    def test_backtracking(self):
+        csp = self.get_csp()
+        australiaColoring = AustraliaColoring(csp)
+        can_be_satisfied = australiaColoring.backtracking_search()
+        print(can_be_satisfied)
+        self.assertTrue(can_be_satisfied)
+
+    @staticmethod
+    def get_csp():
+        X = ['WA', 'NT', 'Q', 'NSW', 'V', 'SA', 'T']
+        D = dict()
+        for x in X:
+            D[x] = ['red', 'green', 'blue']
+        C = {('SA', 'WA'): [('red', 'green'), ('red', 'blue'), ('green', 'red'), ('green', 'blue'), ('blue', 'red'),
+                            ('blue', 'green')],
+             ('SA', 'NT'): [('red', 'green'), ('red', 'blue'), ('green', 'red'), ('green', 'blue'), ('blue', 'red'),
+                            ('blue', 'green')],
+             ('SA', 'Q'): [('red', 'green'), ('red', 'blue'), ('green', 'red'), ('green', 'blue'), ('blue', 'red'),
+                           ('blue', 'green')],
+             ('SA', 'NSW'): [('red', 'green'), ('red', 'blue'), ('green', 'red'), ('green', 'blue'), ('blue', 'red'),
+                             ('blue', 'green')],
+             ('SA', 'V'): [('red', 'green'), ('red', 'blue'), ('green', 'red'), ('green', 'blue'), ('blue', 'red'),
+                           ('blue', 'green')],
+             ('WA', 'NT'): [('red', 'green'), ('red', 'blue'), ('green', 'red'), ('green', 'blue'), ('blue', 'red'),
+                            ('blue', 'green')],
+             ('NT', 'Q'): [('red', 'green'), ('red', 'blue'), ('green', 'red'), ('green', 'blue'), ('blue', 'red'),
+                           ('blue', 'green')],
+             ('Q', 'NSW'): [('red', 'green'), ('red', 'blue'), ('green', 'red'), ('green', 'blue'), ('blue', 'red'),
+                            ('blue', 'green')],
+             ('NSW', 'V'): [('red', 'green'), ('red', 'blue'), ('green', 'red'), ('green', 'blue'), ('blue', 'red'),
+                            ('blue', 'green')]}
+        csp = (X, D, C)
+        return csp
+
+    def test_get_least_constraining_value(self):
+        csp = self.get_csp()
+        australiaColoring = AustraliaColoring(csp)
+        partial_assignment = {'WA': 'red', 'NT': 'green'}
+        next_var = 'Q'
+        actual_least_constraining_value = australiaColoring.get_least_constraining_value(partial_assignment, next_var)
+        expected_least_constraining_value = 'red'
+        self.assertEqual(expected_least_constraining_value, actual_least_constraining_value)
+
+    def test_get_legal_values_left_count(self):
+        csp = self.get_csp()
+        australiaColoring = AustraliaColoring(csp)
+        partial_assignment = {'WA': 'red', 'NT': 'green'}
+        next_var = 'Q'
+
+        extended_assignment = partial_assignment.copy()
+        extended_assignment[next_var] = 'blue'
+        actual_blue_legal_values_left_count = australiaColoring.get_legal_values_left_count(extended_assignment, 'SA')
+        self.assertEqual(actual_blue_legal_values_left_count, 0)
+
+        extended_assignment = partial_assignment.copy()
+        extended_assignment[next_var] = 'red'
+        actual_red_legal_values_left_count = australiaColoring.get_legal_values_left_count(extended_assignment, 'SA')
+        self.assertGreater(actual_red_legal_values_left_count, 0)
